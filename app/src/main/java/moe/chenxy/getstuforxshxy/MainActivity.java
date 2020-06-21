e24509584af733b300d7f07ab95704003aeb42dd
Art_Chen <chenxy0201@qq.com>
2020-06-21 23:31:32 +0800

    UI: Make UI Colorful

--- app/src/main/java/moe/chenxy/getstuforxshxy/MainActivity.java
+++ app/src/main/java/moe/chenxy/getstuforxshxy/MainActivity.java
@@ -32,72 +32,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button login = findViewById(R.id.bt_login);
-        login.setOnClickListener((newValue) -> {
-            new Thread() {
-                public void run() {
-                    try {
-                        // 获取账号和密码
-                        TextInputEditText et_name = findViewById(R.id.et_name);
-                        TextInputEditText et_pass = findViewById(R.id.et_pass);
-                        String name = et_name.getText().toString().trim();
-                        String pass = et_pass.getText().toString().trim();
-                        // 组建post数据
-                        String data = "sid="
-                                + URLEncoder.encode(name, "utf-8") + "&passWord="
-                                + URLEncoder.encode(pass, "utf-8") + "";
-                        //Log.i("Art_Chen", "name : " + name + "pass: " + pass + "data: " + data);
-                        String path = "http://yx.xshxy.cn:802/login";
-                        URL url = new URL(path);
-                        HttpURLConnection conn = (HttpURLConnection) url
-                                .openConnection();
-                        conn.setConnectTimeout(5000);// 5秒
-                        conn.setRequestMethod("POST");
-                        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-                        conn.setRequestProperty("Content-Length", data.length()
-                                + "");
+        login.setOnClickListener((newValue) -> new Thread() {
+            public void run() {
+                try {
+                    // 获取账号和密码
+                    TextInputEditText et_name = findViewById(R.id.et_name);
+                    TextInputEditText et_pass = findViewById(R.id.et_pass);
+                    String name = et_name.getText().toString().trim();
+                    String pass = et_pass.getText().toString().trim();
+                    // 组建post数据
+                    String data = "sid="
+                            + URLEncoder.encode(name, "utf-8") + "&passWord="
+                            + URLEncoder.encode(pass, "utf-8") + "";
+                    //Log.i("Art_Chen", "name : " + name + "pass: " + pass + "data: " + data);
+                    String path = "http://yx.xshxy.cn:802/login";
+                    URL url = new URL(path);
+                    HttpURLConnection conn = (HttpURLConnection) url
+                            .openConnection();
+                    conn.setConnectTimeout(5000);// 5秒
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                    conn.setRequestProperty("Content-Length", data.length()
+                            + "");
 
-                        conn.setDoOutput(true);
-                        conn.getOutputStream().write(data.getBytes());
-                        int code = conn.getResponseCode();
-                        String cookieVal = null;
-                        if (code == 200) {
-                            InputStream in = conn.getInputStream();
-                            cookieVal = conn.getHeaderField("Set-Cookie");
-                            String content = StreamTools.readString(in);
-                            showToast(content);
-                        }
-                        String getStu = "http://yx.xshxy.cn:802/data/getStuForShow";
-                        URL getStuUrl = new URL(getStu);
-
-                        HttpURLConnection conn1 = (HttpURLConnection) getStuUrl
-                                .openConnection();
-                        if (cookieVal != null) {
-                            conn1.setRequestProperty("Cookie", cookieVal); //继承Cookie
-                        }
-                        conn1.setConnectTimeout(5000);
-                        conn1.setRequestMethod("POST");
-                        conn1.setRequestProperty("Referer", "http://yx.xshxy.cn:802/rinfo");
-                        conn1.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                        conn1.setDoOutput(true);
-                        conn1.getOutputStream().write(data.getBytes());
-                        String resultData = "";
-                        if (conn.getResponseCode() == 200) {
-                            InputStream is = conn1.getInputStream();
-                            InputStreamReader isr = new InputStreamReader(is);
-                            BufferedReader bufferReader = new BufferedReader(isr);
-                            String inputLine = "";
-                            while ((inputLine = bufferReader.readLine()) != null) {
-                                resultData += inputLine + "\n";
-                            }
-                            Analysis(resultData);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        showToast("Art_Chen: 连接服务器时发生错误");
+                    conn.setDoOutput(true);
+                    conn.getOutputStream().write(data.getBytes());
+                    int code = conn.getResponseCode();
+                    String cookieVal = null;
+                    if (code == 200) {
+                        InputStream in = conn.getInputStream();
+                        cookieVal = conn.getHeaderField("Set-Cookie");
+                        String content = StreamTools.readString(in);
+                        showToast(content);
                     }
-                };
-            }.start();
-        });
+                    String getStu = "http://yx.xshxy.cn:802/data/getStuForShow";
+                    URL getStuUrl = new URL(getStu);
+
+                    HttpURLConnection conn1 = (HttpURLConnection) getStuUrl
+                            .openConnection();
+                    if (cookieVal != null) {
+                        conn1.setRequestProperty("Cookie", cookieVal); //继承Cookie
+                    }
+                    conn1.setConnectTimeout(5000);
+                    conn1.setRequestMethod("POST");
+                    conn1.setRequestProperty("Referer", "http://yx.xshxy.cn:802/rinfo");
+                    conn1.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                    conn1.setDoOutput(true);
+                    conn1.getOutputStream().write(data.getBytes());
+                    String resultData = "";
+                    if (conn.getResponseCode() == 200) {
+                        InputStream is = conn1.getInputStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader bufferReader = new BufferedReader(isr);
+                        String inputLine = "";
+                        while ((inputLine = bufferReader.readLine()) != null) {
+                            resultData += inputLine + "\n";
+                        }
+                        Analysis(resultData);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    showToast("Art_Chen: 连接服务器时发生错误");
+                }
+            };
+        }.start());
     }
 
     /**
@@ -121,41 +119,32 @@ public class MainActivity extends AppCompatActivity {
         map.put("班级", dataObj.getString("className"));
         map.put("欠费金额(学费)", dataObj.getString("arrearsMoney"));
         list.add(map);
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                AlertDialog.Builder listDialog =
-                        new AlertDialog.Builder(MainActivity.this);
-                listDialog.setTitle("结果");
-                for (HashMap<String, Object> m : list) {
-                    int i = 0;
-                    for (String k : m.keySet()) {
-                        show[i]=(k + " : " + m.get(k));
-                        i++;
-                    }
+        runOnUiThread(() -> {
+            AlertDialog.Builder listDialog =
+                    new AlertDialog.Builder(MainActivity.this);
+            listDialog.setTitle("结果");
+            for (HashMap<String, Object> m : list) {
+                int i = 0;
+                for (String k : m.keySet()) {
+                    show[i]=(k + " : " + m.get(k));
+                    i++;
                 }
-                listDialog.setItems(show, (DialogInterface.OnClickListener) (dialog, which) -> {
-                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    ClipData mClipData = ClipData.newPlainText("Art_Chen", show[which]);
-                    cm.setPrimaryClip(mClipData);
-                    Toast.makeText(MainActivity.this,
-                            "已复制到剪切板",
-                            Toast.LENGTH_SHORT).show();
-                });
-                listDialog.show();
             }
+            listDialog.setItems(show, (DialogInterface.OnClickListener) (dialog, which) -> {
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData mClipData = ClipData.newPlainText("Art_Chen", show[which]);
+                cm.setPrimaryClip(mClipData);
+                Toast.makeText(MainActivity.this,
+                        "已复制到剪切板",
+                        Toast.LENGTH_SHORT).show();
+            });
+            listDialog.show();
         });
 
     }
 
 
     public void showToast(final String content) {
-        runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), content, Toast.LENGTH_LONG).show();
-            }
-        });
+        runOnUiThread(() -> Toast.makeText(getApplicationContext(), content, Toast.LENGTH_LONG).show());
     }
 }
